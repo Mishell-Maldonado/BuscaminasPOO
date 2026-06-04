3e58eabb31c6ea668cf5a1a8a49f589df1c1b1db
AdrianaJurado <ajuradoj@est.ups.edu.ec>
2026-06-03 23:40:41 -0500

    Create CasillaYaDescubiertaException.java

---
+++ src/excepciones/CasillaYaDescubiertaException.java
@@ -0,0 +1,10 @@
+package excepciones;
+
+public class CasillaYaDescubiertaException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public CasillaYaDescubiertaException(String mensaje) {
+        super(mensaje);
+    }
+}
