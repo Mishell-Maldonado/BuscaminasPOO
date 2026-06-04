ca7d4e00d1f00f0320bb16ecdda730a07cb978ea
AdrianaJurado <ajuradoj@est.ups.edu.ec>
2026-06-03 23:35:54 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import controlador.JuegoControlador;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JuegoControlador juego = new JuegoControlador();
+        juego.iniciarJuego();
+    }
+}
